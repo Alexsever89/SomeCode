1f162fa2d95b7d021d6d7852e135a4cfbadff576
Alex Omelchenko <alexsever8993@gmail.com>
2019-01-05 15:59:02 +0300

    added new main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<vector>
+
+template<typename T>
+void Print(const T& container)
+{
+    for(const auto& elem : container)
+        std::cout << elem << " ";
+    std::cout << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello, GitHub!\n";
+    std::vector<int> vi = {1,2,3,4,5};
+    Print(vi);
     return 0;
 }
